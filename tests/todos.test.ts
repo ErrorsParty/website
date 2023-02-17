0c2ae4880cd5cb37b5397169ac6d9b739ddffd73
Jameson <j4m350n@testersen.no>
2023-02-17 14:25:45 +0100

    Added test for deleteTodo() (#9)

--- tests/todos.test.ts
+++ tests/todos.test.ts
@@ -1,8 +1,18 @@
 import { describe, it, expect } from "vitest";
-import { getTodos } from "../src/lib/todos";
+import { deleteTodo, getTodos } from "../src/lib/todos";
+
+let items: Awaited<ReturnType<typeof getTodos>> | undefined;
 
 describe("getTodos", () => {
 	it("should get the todo items", async () => {
-		expect(await getTodos()).toBeInstanceOf(Array);
+		expect((items = await getTodos())).toBeInstanceOf(Array);
+	});
+});
+
+describe("deleteTodos", () => {
+	it("should delete the rows returned from getTodos", async () => {
+		expect(items).not.toBeUndefined();
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		await Promise.all(items!.map(async (todo) => await deleteTodo(todo.id)));
 	});
 });
